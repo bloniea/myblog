c570729af534408f8646f868d044bdced6c378b9
bloniea <bloniea@qq.com>
2022-07-20 04:57:50 +0800

    v

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://api.bloniea.xyz/blogAdmin/api/open/',
-
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
